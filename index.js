135d8b0f97e4870a2c9360fe6bcfa82ae713761b
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-02-04 20:25:17 +0000

    We love copy and paste, finished the shopping cart

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Put your code here
+
+let peonyCount = 0;
+let anemoneCount = 0;
+let daffodilCount = 0;
+
+const peonyPrice = 2;
+const anemonePrice = 4;
+const daffodilPrice = 3;
+let totalPrice = 0;
+
+function updateDisplay() {
+    document.getElementById("result").textContent = totalPrice;
+    document.getElementById("totalPeonys").textContent = peonyCount;
+    document.getElementById("totalAnemones").textContent = anemoneCount;
+    document.getElementById("totalDaffodils").textContent = daffodilCount;
+}
+
+const addPeonyBtn = document.getElementById("addPeony");
+if (addPeonyBtn) {
+    addPeonyBtn.addEventListener("click", () => {
+        peonyCount += 1;
+        totalPrice += peonyPrice;
+        updateDisplay();
+    });
+}
+
+const addAnemoneBtn = document.getElementById("addAnemone");
+if (addAnemoneBtn) {
+    addAnemoneBtn.addEventListener("click", () => {
+        anemoneCount += 1;
+        totalPrice += anemonePrice;
+        updateDisplay();
+    });
+}
+
+const addDaffodilBtn = document.getElementById("addDaffodil");
+if (addDaffodilBtn) {
+    addDaffodilBtn.addEventListener("click", () => {
+        daffodilCount += 1;
+        totalPrice += daffodilPrice;
+        updateDisplay();
+    });
+}
+
+
+
+// initialize displayed values
+updateDisplay();
+
